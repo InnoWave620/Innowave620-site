d99513a45643c6857195e8ba70810a82a36721e6
innowave620 <innowave620@gmail.com>
2025-06-18 16:31:10 +0200

    updated base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-  base: '/Innowave620-site/',
+  plugins: [tailwindcss(), react()],
+  base: "/",
   server: {
-    host: '0.0.0.0', // <--- this is the key part
-    port: 5173       // optional: ensures you always use this port
-  }
-})
+    host: "0.0.0.0", // <--- this is the key part
+    port: 5173, // optional: ensures you always use this port
+  },
+});
